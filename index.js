b0c7a0209bcdff553f74b169ebc71d5b44e7dbbb
Timo Blattner <timoblattner@macbook-pro-de-timo.home>
2019-01-03 12:33:20 +0100

    lot of improvements

--- index.js
+++ index.js
@@ -18,22 +18,33 @@ io.on('connection', function(socket){
   socket.emit("change_html", text)
 
   socket.on('start_game', function(name) {
-    var nx = 100 + Math.random() * (bound.x1 - 100)
-    var ny = 100 + Math.random() * (bound.y1 - 100)
-    var p = {name: name, id: socket.id, x:nx , y: ny, rot: -Math.PI / 2, vx: 0, vy: 0, r: 50, kills: 0, killed: 0, health: 100, color: get_random_color(), shoot_countdown: 0}
-    players.push(p)
+    if (name.length > 0 && name.length <= 10){
+      var nx = 100 + Math.random() * (bound.x1 - 100)
+      var ny = 100 + Math.random() * (bound.y1 - 100)
+      var p = {name: name, id: socket.id, x:nx , y: ny, rot: -Math.PI / 2, vx: 0, vy: 0, r: 50, kills: 0, killed: 0, health: 100, color: get_random_color(), shoot_countdown: 0, input_r: 0}
+      players.push(p)
 
-    var text = get_canvas()
-    text += get_game_logic()
+      var text = get_canvas()
+      text += get_game_logic()
 
-    socket.emit("change_html", text)
+      socket.emit("change_html", text)
+    }
   })
 
-  socket.on('change_input', function(vx, vy, rot) {
+  socket.on('change_rotation_input', function(rot) {
     var player = players.filter(obj => {
       return obj.id === socket.id
     })
-    player[0] = change_input(player[0], vx, vy, rot)
+    player[0].input_r = rot
+  });
+
+  socket.on('change_y_input', function(vy) {
+    var player = players.filter(obj => {
+      return obj.id === socket.id
+    })
+    player[0].vx += vy * Math.cos(player[0].rot) * 0.5
+    player[0].vy += vy * Math.sin(player[0].rot) * 0.5
+
   });
 
   socket.on('shoot', function() {
@@ -53,17 +64,18 @@ io.on('connection', function(socket){
   });
 });
 
-var bound = {x0: 0, y0: 0, x1: 1000, y1: 1000}
+var bound = {x0: 0, y0: 0, x1: 2000, y1: 1000}
 var bounciness = 0.75
 var players = []
 var bullets = []
 var shoot_freq = 30
-var shoot_force = 10
+var shoot_force = 30
 
 setInterval(() => {
   if (players.length > 0){
     [players, bullets] = update_map(players, bullets)
     [players, bullets] = move(players, bullets)
+    players = sort_player_list(players)
     io.sockets.emit('update', players, bullets);
   }
 }, 1000 / 60);
@@ -72,35 +84,69 @@ function shoot(player){
   player.shoot_countdown = shoot_freq
   var vx = player.vx + Math.cos(player.rot) * shoot_force
   var vy = player.vy + Math.sin(player.rot) * shoot_force
-  var bullet = {x: player.x, y: player.y, vx: vx, vy: vy, id: player.id}
+  var bullet = {x: player.x, y: player.y, r:5, vx: vx, vy: vy, id: player.id, state:"shot"}
   bullets.push(bullet)
 }
 
+function sort_player_list(players){
+  for (var i = 0; i < players.length - 1; i++){
+    if ((players[i].kills - players[i].killed) < (players[i + 1].kills - players[i + 1].killed)){
+      var temp = players[i]
+      players[i] = players[i + 1]
+      players[i + 1] = temp
+    } else if ((players[i].kills - players[i].killed) === (players[i + 1].kills - players[i + 1].killed)){
+      if (players[i].kills < players[i].killed){
+        var temp = players[i]
+        players[i] = players[i + 1]
+        players[i + 1] = temp
+      }
+    }
+  }
+  return players
+}
+
 function get_menu(){
-  var menu = `<input id="name_input" placeholder="Name: ">
-              <button onclick="socket.emit(\'start_game\', document.getElementById('name_input').value)">Start Game</button>`
+  var menu = `
+    <center>
+    <h1>Asteroid Multiplayer Game</h1>
+    <i>C'est un premier essai pour mon tm</i><br><br>
+    <input id="name_input" placeholder="Name: ">
+    <button onclick="socket.emit(\'start_game\', document.getElementById('name_input').value)">Start Game</button>
+    </center> `
   return menu
 }
 
 function get_canvas(){
-  var canvas = '<canvas id="myCanvas"></canvas>'
+  var canvas = `
+  <canvas id="myCanvas" style="position: absolute;"></canvas>
+  <div id="player_container" style="width: 150px; right: 0px; top: 0px; position: absolute; background-color: rgba(102, 102, 102, 0.5)">
+    <div id="player_list" style="color: white; margin: 5px"></div>
+  </div>
+  `
   return canvas
 }
 
 function move(players, bullets){
   for (var i = 0; i < players.length; i++){
+    players[i].rot += (players[i].input_r / 10)
     players[i].x += players[i].vx
     players[i].y += players[i].vy
   }
 
   for (var i = bullets.length - 1; i >= 0; i--){
-    bullets[i].x += bullets[i].vx
-    bullets[i].y += bullets[i].vy
-    if (bullets[i].x < bound.x0 || bullets[i].x > bound.x1 || bullets[i].y < bound.y0 || bullets[i].y > bound.y1){
-      bullets.splice(i, 1)
+    if (bullets[i].state == "shot"){
+      bullets[i].x += bullets[i].vx
+      bullets[i].y += bullets[i].vy
+      if (bullets[i].x < bound.x0 || bullets[i].x > bound.x1 || bullets[i].y < bound.y0 || bullets[i].y > bound.y1){
+        bullets.splice(i, 1)
+      }
+    } else if (bullets[i].state == "exploded") {
+      bullets[i].r += 0.5
+      if (bullets[i].r > 10){
+        bullets.splice(i, 1)
+      }
     }
   }
-
   return [players, bullets]
 }
 
@@ -130,6 +176,7 @@ function update_map(players, bullets){
 
           players[j].vx = nvx1
           players[j].vy = nvy1
+
           players[j].health -= damage
           if (players[j].health <= 0){
             players[j].health = 100
@@ -173,18 +220,20 @@ function update_map(players, bullets){
     }
 
     for (var i = bullets.length - 1; i >= 0; i--) {
-      var d = Math.pow(players[j].x - bullets[i].x, 2) + Math.pow(players[j].y - bullets[i].y, 2)
-      if (d < (players[j].r * players[j].r) && bullets[i].id !== players[j].id){
-        players[j].health -= 10
-        if (players[j].health <= 0){
-          players[j].health = 100
-          var player = players.filter(obj => {
-            return obj.id === bullets[i].id
-          })
-          players[j].killed += 1
-          player[0].kills += 1
+      if (bullets[i].state == "shot"){
+        var d = Math.pow(players[j].x - bullets[i].x, 2) + Math.pow(players[j].y - bullets[i].y, 2)
+        if (d < (players[j].r * players[j].r) && bullets[i].id !== players[j].id){
+          players[j].health -= 10
+          if (players[j].health <= 0){
+            players[j].health = 100
+            var player = players.filter(obj => {
+              return obj.id === bullets[i].id
+            })
+            players[j].killed += 1
+            player[0].kills += 1
+          }
+          bullets[i].state = "exploded"
         }
-        bullets.splice(i, 1)
       }
     }
   }
@@ -207,18 +256,21 @@ function get_game_logic(){
       c.width = window.innerWidth
       c.height = window.innerHeight
       ctx = c.getContext('2d')
-      bound = {x0: 0, y0: 0, x1: 1000, y1: 1000}
+      bound = {x0: ${bound.x0}, y0: ${bound.y0}, x1: ${bound.x1}, y1: ${bound.y1}}
 
       document.addEventListener('keydown', function(event) {
-        if (event.keyCode == "68"){socket.emit('change_input', 0, 0, 1)} //d
-        if (event.keyCode == "65"){socket.emit('change_input', 0, 0, -1)} //a
-        if (event.keyCode == "87"){socket.emit('change_input', 0, 1, 0)} //w
-        if (event.keyCode == "83"){socket.emit('change_input', 0, -1, 0)} //s
+        if (event.keyCode == "68"){socket.emit('change_rotation_input', 1)} //d
+        if (event.keyCode == "65"){socket.emit('change_rotation_input', -1)} //a
+        if (event.keyCode == "87"){socket.emit('change_y_input', 1)} //w
+        if (event.keyCode == "83"){socket.emit('change_y_input', -1)} //s
         if (event.keyCode == "32"){socket.emit('shoot')} //s
       }, false)
 
       document.addEventListener('keyup', function(event) {
-        socket.emit('change_input', 0, 0, 0)
+        if (event.keyCode == "68"){socket.emit('change_rotation_input', 0)} //d
+        if (event.keyCode == "65"){socket.emit('change_rotation_input', 0)} //a
+        if (event.keyCode == "87"){socket.emit('change_y_input', 0)} //w
+        if (event.keyCode == "83"){socket.emit('change_y_input', 0)} //s
       }, false)
 
       socket.on('update', function(players, bullets) {
@@ -233,7 +285,7 @@ function get_game_logic(){
         var dx = parseInt(c.width / 2) - player.x
         var dy = parseInt(c.height / 2) - player.y
 
-        border = 200
+        border = 100
 
         for (var i = 0; i < players.length; i++){
           var x = dx + players[i].x
@@ -241,11 +293,12 @@ function get_game_logic(){
           if (x >= -border && x <= c.width + border && y >= -border && y <= c.width + border){
             draw(x, y, players[i].r, players[i].rot, players[i].color)
             display_player_info(players[i], x, y)
-            draw_borders(dx, dy)
           }
         }
+        draw_borders(dx, dy)
         draw_bullets(dx, dy, bullets)
         draw_mini_map(players)
+        display_player_rank(players)
       })
 
       function draw_bullets(dx, dy, bullets){
@@ -255,7 +308,8 @@ function get_game_logic(){
           var y = dy + bullets[i].y
           if (x >= 0 && x <= c.width && y >= 0 && y <= c.width){
             ctx.beginPath();
-            ctx.arc(x, y, 3, 0, 2 * Math.PI);
+            ctx.arc(x, y, bullets[i].r, 0, 2 * Math.PI);
+            ctx.lineWidth = 0;
             ctx.fill()
           }
         }
@@ -269,7 +323,7 @@ function get_game_logic(){
 
         ctx.globalAlpha = 1.0
         ctx.fillStyle = color
-        var path = [[0, 10], [r/2, r/2], [0, -r/2], [-r/2, r/2], [0, 10]]
+        var path = [[0, 10], [r/2, r/2], [0, -2 * r / 3], [-r/2, r/2], [0, 10]]
         ctx.moveTo(x, y)
         ctx.beginPath() 
         for (var i = 0; i < path.length; i++){
@@ -310,16 +364,13 @@ function get_game_logic(){
       }
 
       function draw_mini_map(players){
-        var w = parseInt(c.width / 6)
-        var h = parseInt(c.height / 6)
-        ctx.globalAlpha = 1.0
+        var w = parseInt(bound.x1 / 10)
+        var h = parseInt(bound.y1 / 10)
         
-        ctx.fillStyle = "white"
+        ctx.globalAlpha = 1.0
+        //ctx.clearRect(0, 0, w, h);
+        ctx.fillStyle = "rgba(102, 102, 102, 0.5)"
         ctx.fillRect(0, 0, w, h);
-
-        ctx.strokeStyle = "black"
-        ctx.rect(0, 0, w, h);
-        ctx.stroke();
 
         var sx = w / bound.x1
         var sy = h / bound.y1
@@ -330,17 +381,21 @@ function get_game_logic(){
           ctx.fill()
         }
       }
+
+      function display_player_rank(players){
+        var text = "<table><tr><th>Player</th><th>Kills</th><th>Killed</th></tr>"
+        for (var i = 0; i < players.length; i++){
+          text += "<tr><td>" + players[i].name + "</td><td>" + players[i].kills.toString() + "</td><td>" + players[i].killed.toString() + "</td><tr>"
+        }
+        text += "</table>"
+        $("#player_list").html(text)
+      }
+
+      socket.on('disconnect', function() {
+        var text = "Reload page..."
+        $("body").html(text)
+      });
     </script>`
 
   return logic
-}
-
-function change_input (player, vx, vy, rot){
-  player.rot += (rot * 0.3)
-  if (vy !== 0){
-    //forward
-    player.vx += vy * Math.cos(player.rot) * 0.5
-    player.vy += vy * Math.sin(player.rot) * 0.5
-  }
-  return player
 }
